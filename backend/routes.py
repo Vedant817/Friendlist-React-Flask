8f9fa65d6272786b22538de45da5dfa0e9cc5cb8
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-05-31 19:04:34 +0530

    Build creating and deleting Route

--- backend/routes.py
+++ backend/routes.py
@@ -8,3 +8,52 @@ def get_friends():
     friends = Friend.query.all()
     result = [friend.to_json() for friend in friends]
     return jsonify(result)
+
+#? Create a friend
+@app.route("/api/friends", methods=["POST"])
+def create_friend():
+    try:
+        data = request.json
+        
+        required_fields = ["name", "role", "description", "gender"]
+        for field in required_fields:
+            if field not in data:
+                return jsonify({"error": f"Missing Required Field: {field}"}), 400  
+        
+        name = data.get("name")
+        role = data.get("role")
+        description = data.get("description")
+        gender = data.get("gender")
+        
+        #? Fetching Avatar Image based upon gender
+        if gender == "male":
+            img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+        elif gender == "female":
+            img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+        else:
+            img_url = None
+            
+        new_friend = Friend(name=name, role=role, description=description, gender=gender, img_url=img_url)
+        db.session.add(new_friend)
+        db.session.commit()
+        
+        return jsonify(new_friend.to_json()), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+#? Deleting a Friend
+@app.route("/api/friends/<int:id>", methods=["DELETE"])
+def delete_friend(id):
+    try:
+        friend = Friend.query.get(id)
+        
+        if friend is None:
+            return jsonify({"error": "Friend Not Found"}), 404
+        
+        db.session.delete(friend)
+        db.session.commit()
+        return jsonify({"msg": "Friend Deleted"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
