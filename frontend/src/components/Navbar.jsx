7eb0b312dfd749cb3ab857edae87c799d42d911a
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-05-31 22:00:25 +0530

    Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Box, Button, Container, Flex, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
+const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode()
+    return (
+        <Container maxW={"900px"}>
+            <Box px={4} my={4} borderRadius={5} bg={useColorModeValue("gray.200", "gray.700")}>
+                <Flex h="16" alignItems={"center"} justifyContent={"space-between"}>
+                    {/*Left Flex*/}
+                    <Flex alignItems={"center"} justifyContent={"center"} gap={3} display={{base: "none", sm: "flex"}}>
+                        <img src='/react.png' alt='react-logo' width={50} height={50} />
+                        <Text fontSize={"40px"}>+</Text>
+                        <img src='/python.png' alt='python-logo' width={50} height={40} />
+                        <Text fontSize={"40px"}>=</Text>
+                        <img src='/explode.png' alt='explode-head' width={45} height={45} />
+                    </Flex>
+                    {/*Right Flex*/}
+                    <Flex gap={3} align={"center"}>
+                        <Text fontSize={"lg"} fontWeight={500} display={{base: "none", md: "block"}}>
+                            BFFShip 🔥
+                        </Text>
+                        <Button onClick={toggleColorMode}>
+                            {colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+                        </Button>
+                    </Flex>
+                </Flex>
+            </Box>
+        </Container>
+    )
+}
+
+export default Navbar
