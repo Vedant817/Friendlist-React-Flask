350db40c62d692a8f2e3ae309a3bd5df37d1d509
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-05-31 22:45:22 +0530

    Navbar Completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Box, Button, Container, Flex, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
+import CreateUserModel from './CreateUserModel';
 
 const Navbar = () => {
     const { colorMode, toggleColorMode } = useColorMode()
@@ -24,6 +25,7 @@ const Navbar = () => {
                         <Button onClick={toggleColorMode}>
                             {colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
                         </Button>
+                        <CreateUserModel />
                     </Flex>
                 </Flex>
             </Box>
